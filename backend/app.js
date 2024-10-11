5d882eedc9fa297293ce852e0c0c32be82ce0d6b
it22599568 <128208008+it22599568@users.noreply.github.com>
2024-10-11 13:54:22 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+// Routers
+const itemRouter = require("./Routes/ItemRoute");
+
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/items", itemRouter); // localhost:5000/items
+
+
+// Configuration
+const PORT = process.env.PORT || 8080;
+const MONGO_URL =
+  process.env.MONGO_URL ||
+  "mongodb+srv://admin:3NcXuTagx2XuBxWT@cluster0.uipeljr.mongodb.net/"; 
+const DEV_MODE = process.env.DEV_MODE || "development";
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the application if unable to connect to MongoDB
+  });
---
+++ backend/frontend/src/App.js
@@ -0,0 +1,30 @@
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// Admin-Item Adding
+import AddItem from "./components/Renatal System/Admin/Add Item/AddItem";
+import AdminItems from "./components/Renatal System/Admin/Item/Items";
+import UpdateItem from "./components/Renatal System/Admin/Item/UpdateItem";
+
+//User-Item Display,Order
+import UserItems from "./components/Renatal System/User/Item/Items";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          {/* Admin-Item Adding */}
+          <Route exact path="/add-items" element={<AddItem />} />
+          <Route exact path="/admin-items" element={<AdminItems />} />
+          <Route path="/update-item/:id" element={<UpdateItem />} />
+
+          {/* User-Item Display  */}
+          <Route exact path="/" element={<UserItems />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
